6dcdd676df39cbd1f8f1b647f94a9c3b2de1e8b5
dexter <sohambasak42@gmail.com>
2023-06-21 19:03:13 +0530

    stage three

--- Filter/script.js
+++ Filter/script.js
@@ -1,1 +1,67 @@
-console.log("Hello world!");
+const data = [
+    {
+      id: 1,
+      name: "Invicta Men's Pro Diver",
+      img: "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
+      price: 74,
+      cat: "Dress",
+    },
+    {
+      id: 11,
+      name: "Invicta Men's Pro Diver 2",
+      img: "https://m.media-amazon.com/images/I/71e04Q53xlL._AC_UY879_.jpg",
+      price: 74,
+      cat: "Dress",
+    },
+    {
+      id: 2,
+      name: "Timex Men's Expedition Scout ",
+      img: "https://m.media-amazon.com/images/I/91WvnZ1g40L._AC_UY879_.jpg",
+      price: 40,
+      cat: "Sport",
+    },
+    {
+      id: 3,
+      name: "Breitling Superocean Heritage",
+      img: "https://m.media-amazon.com/images/I/61hGDiWBU8L._AC_UY879_.jpg",
+      price: 200,
+      cat: "Luxury",
+    },
+    {
+      id: 4,
+      name: "Casio Classic Resin Strap ",
+      img: "https://m.media-amazon.com/images/I/51Nk5SEBARL._AC_UY879_.jpg",
+      price: 16,
+      cat: "Sport",
+    },
+    {
+      id: 5,
+      name: "Garmin Venu Smartwatch ",
+      img: "https://m.media-amazon.com/images/I/51kyjYuOZhL._AC_SL1000_.jpg",
+      price: 74,
+      cat: "Casual",
+    },
+];
+
+
+const productsContainer = document.querySelector(".products")
+const searchInput = document.querySelector(".search")
+const categories = document.querySelector(".categories")
+const priceRange = document.querySelector(".priceRange")
+const priceValue = document.querySelector(".priceValue")
+
+
+const displayProducts = (filterProducts) => {
+    productsContainer.innerHTML = filterProducts.map(product =>
+        `
+        <div class="product">
+            <img src= ${product.img} alt=""> 
+            <span class="name">${product.name}</span>
+            <span class="priceText">$${product.price}</span>
+        </div>
+
+        `
+    ).join("");
+};
+
+displayProducts(data);
